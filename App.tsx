4b8bf8db8a5c18cc0f4c33eff70d9c254e4f9689
rodrigo teixeira silva <r.teixeira1@outlook.com.br>
2023-08-11 22:24:05 -0300

    Botão

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { ThemeProvider} from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import theme from './src/theme';
 
 import SignIn from './src/screens/SignIn';
 
-import theme from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 import { StatusBar } from 'react-native';
 
@@ -13,18 +13,20 @@ export default function App() {
 
     if (!fontsLoaded) {
         return(
-        
             <Loading />
         );
     }
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={ theme }>
+
     <StatusBar 
     barStyle='light-content' 
     backgroundColor="transparent" 
     translucent />
+
     <SignIn/>
+
     </ThemeProvider>
   );
 }
--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,8 @@
 import 'styled-components';
 import theme from '../theme';
 
-declare module 'style-components'{
+declare module 'styled-components'{
     type ThemeType = typeof theme;
 
-    export interface defaultTheme extends ThemeType{}
+    export interface DefaultTheme extends ThemeType{}
 }
